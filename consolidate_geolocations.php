a6019ccdf7ef778c7fa0a818c59c1b6fc78a3c6d
oskarvedel <oskar.vedel@gmail.com>
2023-12-10 13:07:58 +0100

    maybe fixed get_nearest_geolocations_with_gd_places

--- consolidate_geolocations.php
+++ consolidate_geolocations.php
@@ -263,42 +263,6 @@ function send_email($body, $subject)
     wp_mail($to, $subject, $body, $headers);
 }
 
-function get_nearest_geolocations_with_gd_places_for_single_geolocation($geolocations_ids, $current_geolocation_id, $limit = 5)
-{
-    $distances = array();
-    $current_geodir_title = get_the_title($current_geolocation_id);
-
-    $current_lat = get_post_meta($current_geolocation_id, 'latitude', true);
-    $current_long = get_post_meta($current_geolocation_id, 'longitude', true);
-
-    foreach ($geolocations_ids as $geolocation_id) {
-        $gd_places = get_post_meta($geolocation_id, 'gd_place_list', true);
-        if (empty($gd_places)) {
-            continue;
-        }
-        unset($gd_places);
-
-        $lat = get_post_meta($geolocation_id, 'latitude', true);
-        $long = get_post_meta($geolocation_id, 'longitude', true);
-
-
-        $theta = $current_long - $long;
-
-        $dist = sin(deg2rad($current_lat)) * sin(deg2rad($lat)) +  cos(deg2rad($current_lat)) * cos(deg2rad($lat)) * cos(deg2rad($theta));
-        $dist = acos($dist);
-        $dist = rad2deg($dist);
-        $kilometers = $dist * 60 * 1.852;
-
-        $distances[$geolocation_id] = $kilometers;
-        unset($lat, $long, $kilometers, $theta, $dist);
-    }
-
-    asort($distances);
-    $distances = array_slice($distances, 0, $limit, true);
-
-    return $distances;
-}
-
 function set_nearest_geolocations_with_gd_places_for_all_geolocations()
 {
     // $paged = 1;
@@ -335,16 +299,53 @@ function set_nearest_geolocations_with_gd_places_for_all_geolocations()
         return;
     }
 
+
     foreach ($geolocations_ids as $geolocation_id) {
         $nearest_geolocations = get_nearest_geolocations_with_gd_places_for_single_geolocation($geolocations_ids, $geolocation_id, 5);
 
-        $nearest_geolocations_keys = array_keys($nearest_geolocations);
+        $nearest_geolocation_ids = array_keys($nearest_geolocations);
+        update_post_meta($geolocation_id, 'nearest_geolocations', $nearest_geolocation_ids);
 
-        // Set the "nearest_geolocations" metadata field
-        update_post_meta($geolocation_id, 'nearest_geolocations', $nearest_geolocations_keys);
-        trigger_error("nearest geolocations set for geolocation: " . get_the_title($geolocation_id), E_USER_WARNING);
+        break;
         unset($nearest_geolocations, $nearest_geolocations_keys);
     }
+}
+
+function get_nearest_geolocations_with_gd_places_for_single_geolocation($geolocations_ids, $current_geolocation_id, $limit = 5)
+{
+    $distances = array();
+    $current_geodir_title = get_the_title($current_geolocation_id);
+
+    $current_lat = get_post_meta($current_geolocation_id, 'latitude', true);
+    $current_long = get_post_meta($current_geolocation_id, 'longitude', true);
+
+    $geolocations_ids = array_diff($geolocations_ids, [$current_geolocation_id]);
+
+    foreach ($geolocations_ids as $geolocation_id) {
+        $gd_places = get_post_meta($geolocation_id, 'gd_place_list', true);
+        if (empty($gd_places)) {
+            continue;
+        }
+        unset($gd_places);
+
+        $lat = get_post_meta($geolocation_id, 'latitude', true);
+        $long = get_post_meta($geolocation_id, 'longitude', true);
+
+        $theta = $current_long - $long;
+
+        $dist = sin(deg2rad($current_lat)) * sin(deg2rad($lat)) +  cos(deg2rad($current_lat)) * cos(deg2rad($lat)) * cos(deg2rad($theta));
+        $dist = acos($dist);
+        $dist = rad2deg($dist);
+        $kilometers = $dist * 60 * 1.852;
+
+        $distances[$geolocation_id] = $kilometers;
+        unset($lat, $long, $kilometers, $theta, $dist);
+    }
+
+    asort($distances);
+    $distances = array_slice($distances, 0, $limit, true);
+
+    return $distances;
 }
 
 // function correct_parent_locations($geodir_post_neighbourhoods, $geodir_post_locations, $geodir_post_neighbourhoods_ids, $geolocations_ids)
