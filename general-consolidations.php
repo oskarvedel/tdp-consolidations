8dfa802dc02ea9839e7920595775d021b16d8edc
oskarvedel <oskar.vedel@gmail.com>
2023-12-22 12:56:02 +0100

    added add_neighbourhoods_gd_places_to_gd_place_list

--- general-consolidations.php
+++ general-consolidations.php
@@ -3,7 +3,7 @@
 function general_consolidations()
 {
     find_duplicate_geolocations();
-    add_neighbourhoods_gd_places_to_gd_place_list();
+    add_gd_places_from_neighbourhoods_to_gd_place_list();
     update_num_of_direct_gd_places();
     trigger_error("general consolidations done", E_USER_NOTICE);
 }
@@ -41,21 +41,21 @@ function update_num_of_direct_gd_places()
 }
 
 
-function add_neighbourhoods_gd_places_to_gd_place_list()
+function add_gd_places_from_neighbourhoods_to_gd_place_list()
 {
-    xdebug_break();
     $geolocations_ids = get_posts(array('post_type' => 'geolocations', 'posts_per_page' => -1, 'fields' => 'ids'));
     foreach ($geolocations_ids as $current_geolocation_id) {
         $neighbourhoods = get_post_meta($current_geolocation_id, 'geodir_neighbourhoods', false);
-        $neighbourhoods_gd_place_ids = array();
+        $all_neighbourhoods_gd_place_ids = array();
         foreach ($neighbourhoods as $neighbourhood) {
             $neighbourhood_gd_place_ids = get_post_meta($neighbourhood, 'gd_place_list', false);
-            $neighbourhoods_gd_place_ids = array_merge($neighbourhoods_gd_place_ids, $neighbourhood_gd_place_ids);
+            $all_neighbourhoods_gd_place_ids = array_merge($all_neighbourhoods_gd_place_ids, $neighbourhood_gd_place_ids);
         }
-        $neighbourhoods_gd_place_ids = array_unique($neighbourhoods_gd_place_ids);
-        $gd_place_ids_list = get_post_meta($current_geolocation_id, 'gd_place_list', false);
-        $gd_place_ids_list = array_merge($gd_place_ids_list, $neighbourhoods_gd_place_ids);
-        $gd_place_ids_list = array_unique($gd_place_ids_list);
-        update_post_meta($current_geolocation_id, 'gd_place_list', $gd_place_ids_list);
+        $all_neighbourhoods_gd_place_ids = array_unique($all_neighbourhoods_gd_place_ids);
+        $current_gd_place_ids_list = get_post_meta($current_geolocation_id, 'gd_place_list', false);
+
+        $new_gd_place_ids_list = array_merge($current_gd_place_ids_list, $all_neighbourhoods_gd_place_ids);
+        $new_gd_place_ids_list = array_unique($new_gd_place_ids_list);
+        update_post_meta($current_geolocation_id, 'gd_place_list', $new_gd_place_ids_list);
     }
 }
