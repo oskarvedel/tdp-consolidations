027ef493c19c439b103e6602b33a1ec3103aa19e
oskarvedel <oskar.vedel@gmail.com>
2023-12-24 00:03:24 +0100

    made get_gd_places_in_neighbourhoods into a func

--- seo-consolidations.php
+++ seo-consolidations.php
@@ -173,45 +173,15 @@ function get_gd_places_within_radius($geolocation, $radius)
 //seo gd place list is gd_places_within_8_km + geodir_neighbourhoods_gd_place_list
 function generate_seo_gd_place_list_for_all_geolocations()
 {
-    global $wpdb;
-    $geodir_gd_place_detail_table = $wpdb->get_results("SELECT * FROM {$wpdb->prefix}geodir_gd_place_detail", OBJECT);
-
     $geolocations_ids = get_posts(array('post_type' => 'geolocations', 'posts_per_page' => -1, 'fields' => 'ids'));
     foreach ($geolocations_ids as $geolocation_id) {
         $gd_places_within_8_km = get_gd_places_within_radius($geolocation_id, 8);
-        // $gd_places_ids_within_8_km = array_keys($gd_places_ids_within_8_km);
 
-        $gd_places_in_neighbourhoods = [];
-
-        // $gd_neighbourhoods = get_post_meta($geolocation_id, 'geodir_neighbourhoods', false);
-        // if (!empty($gd_neighbourhoods)) {
-        //     $gd_neighbourhoods_gd_place_ids = array();
-        //     foreach ($gd_neighbourhoods as $gd_neighbourhood) {
-        //         $gd_neighbourhood_gd_place_ids = get_post_meta($gd_neighbourhood, 'gd_place_list', false);
-        //         $gd_neighbourhoods_gd_place_ids = array_merge($gd_neighbourhoods_gd_place_ids, $gd_neighbourhood_gd_place_ids);
-        //     }
-        //     $gd_neighbourhoods_gd_place_ids = array_unique($gd_neighbourhoods_gd_place_ids);
-
-        //     $filtered_geodir_gd_place_detail_table = [];
-        //     foreach ($gd_neighbourhoods_gd_place_ids as $gd_place_id) {
-        //         foreach ($geodir_gd_place_detail_table as $gd_place_detail) {
-        //             if ($gd_place_detail->post_id == $gd_place_id) {
-        //                 $filtered_geodir_gd_place_detail_table[] = $gd_place_detail;
-        //             }
-        //         }
-        //     }
-
-        //     $gd_places_in_neighbourhoods = [];
-        //     foreach ($filtered_geodir_gd_place_detail_table as $gd_place) {
-        //         $distance = find_distance_from_geolocation($geolocation_id, $gd_place);
-        //         $gd_places_in_neighbourhoods[$gd_place->post_id] = $distance;
-        //     }
-        // }
-
+        $gd_places_in_neighbourhoods = get_gd_places_in_neighbourhoods($geolocation_id);
 
         $seo_gd_place_list = [];
 
-        // $seo_gd_place_list = $gd_places_within_8_km + $gd_places_in_neighbourhoods;
+        $seo_gd_place_list = $gd_places_within_8_km + $gd_places_in_neighbourhoods;
 
         $seo_gd_place_list = array_unique($seo_gd_place_list);
         asort($seo_gd_place_list);
@@ -220,6 +190,50 @@ function generate_seo_gd_place_list_for_all_geolocations()
 
         update_post_meta($geolocation_id, 'seo_gd_place_list', $seo_gd_place_list);
     }
+}
+
+function get_gd_places_in_neighbourhoods($geolocation_id)
+{
+    global $wpdb;
+    $geodir_gd_place_detail_table = $wpdb->get_results("SELECT * FROM {$wpdb->prefix}geodir_gd_place_detail", OBJECT);
+
+    //get neighbourhoods
+    $gd_neighbourhoods = get_post_meta($geolocation_id, 'geodir_neighbourhoods', false);
+    if (empty($gd_neighbourhoods)) {
+        return [];
+    }
+
+    //get gd_place_ids in neighbourhoods
+    $gd_neighbourhoods_gd_place_ids = array();
+    foreach ($gd_neighbourhoods as $gd_neighbourhood) {
+        $gd_neighbourhood_gd_place_ids = get_post_meta($gd_neighbourhood, 'gd_place_list', false);
+        $gd_neighbourhoods_gd_place_ids = array_merge($gd_neighbourhoods_gd_place_ids, $gd_neighbourhood_gd_place_ids);
+    }
+    if (empty($gd_neighbourhoods_gd_place_ids)) {
+        return [];
+    }
+    $gd_neighbourhoods_gd_place_ids = array_unique($gd_neighbourhoods_gd_place_ids);
+
+    //populate filtered_geodir_gd_place_detail_table
+    $filtered_geodir_gd_place_detail_table = [];
+    foreach ($gd_neighbourhoods_gd_place_ids as $gd_place_id) {
+        foreach ($geodir_gd_place_detail_table as $gd_place_detail) {
+            if ($gd_place_detail->post_id == $gd_place_id) {
+                $filtered_geodir_gd_place_detail_table[] = $gd_place_detail;
+            }
+        }
+    }
+    if (empty($filtered_geodir_gd_place_detail_table)) {
+        return [];
+    }
+
+    //find distance from geolocation for each gd_place
+    $gd_places_in_neighbourhoods = [];
+    foreach ($filtered_geodir_gd_place_detail_table as $gd_place) {
+        $distance = find_distance_from_geolocation($geolocation_id, $gd_place);
+        $gd_places_in_neighbourhoods[$gd_place->post_id] = $distance;
+    }
+    return $gd_places_in_neighbourhoods;
 }
 
 function find_distance_from_geolocation($geolocation, $gd_place)
